89c7d65cbf1801fba1e6cb06c0c2a7fb17745ef2
punty <francesco.puntillo@gmail.com>
2018-12-13 12:44:30 +0000

    Add isBipartite for not strongly connected graph

--- SwiftBasicAlgorithms/Sources/Algorithms/BipartiteGraph.swift
+++ SwiftBasicAlgorithms/Sources/Algorithms/BipartiteGraph.swift
@@ -15,17 +15,12 @@ class BipartiteGraph<T: Hashable> {
     init (graph: Graph<T>) {
         self.graph = graph
         visited = Array<Bool>(repeating: false, count: graph.count)
-        
     }
     
-    func isBipartite() -> Bool {
-        var color = Array<Int>(repeating: -1, count: graph.count)
-        if graph.count <= 4 {
-            return graph.count == 2
-        }
-        color[0] = 1
+    func isBipartite(start: Int, color:inout [Int]) -> Bool {
+        color[start] = 1
         var queue = Queue<Int>()
-        queue.enqueue(0)
+        queue.enqueue(start)
         while let idx = queue.dequeue() {
             for edge in graph[idx].edges {
                 if color[edge] < 0 {
@@ -35,6 +30,21 @@ class BipartiteGraph<T: Hashable> {
                     if color[edge] == color[idx] {
                         return false
                     }
+                }
+            }
+        }
+        return true
+    }
+    
+    func isBipartite() -> Bool {
+        if graph.count == 0 {
+            return true
+        }
+        var color = Array<Int>(repeating: -1, count: graph.count)
+        for idx in graph.vertexesIndex {
+            if color[idx] < 0 {
+                if !isBipartite(start: idx, color: &color) {
+                    return false
                 }
             }
         }
--- SwiftBasicAlgorithms/Tests/BipartiteGraphTests.swift
+++ SwiftBasicAlgorithms/Tests/BipartiteGraphTests.swift
@@ -40,5 +40,50 @@ class BipartiteGraphTests: XCTestCase {
         let b = BipartiteGraph(graph: simpleGraph)
         XCTAssertTrue(b.isBipartite())
     }
+    
+    func testEmptyGraph() {
+        let simpleGraph = Graph<Int>()
+        let b = BipartiteGraph(graph: simpleGraph)
+        XCTAssertTrue(b.isBipartite())
+    }
+    
+    func testNotStronglyConnectedGraph() {
+        var simpleGraph = Graph<Int>()
+        let v0 = simpleGraph.createVertex(data: 0)
+        let v1 = simpleGraph.createVertex(data: 1)
+        let v2 = simpleGraph.createVertex(data: 2)
+        let v3 = simpleGraph.createVertex(data: 3)
+        let v4 = simpleGraph.createVertex(data: 4)
+        let v5 = simpleGraph.createVertex(data: 5)
+        let v6 = simpleGraph.createVertex(data: 6)
+        simpleGraph.createEdges(from: v0, to: v3)
+        simpleGraph.createEdges(from: v1, to: v3)
+        simpleGraph.createEdges(from: v2, to: v4)
+        simpleGraph.createEdges(from: v2, to: v3)
+        simpleGraph.createEdges(from: v5, to: v6)
+        let b = BipartiteGraph(graph: simpleGraph)
+        XCTAssertTrue(b.isBipartite())
+    }
 
+    func testNotStronglyConnectedGraphNotBipartite() {
+        var simpleGraph = Graph<Int>()
+        let v0 = simpleGraph.createVertex(data: 0)
+        let v1 = simpleGraph.createVertex(data: 1)
+        let v2 = simpleGraph.createVertex(data: 2)
+        let v3 = simpleGraph.createVertex(data: 3)
+        let v4 = simpleGraph.createVertex(data: 4)
+        let v5 = simpleGraph.createVertex(data: 5)
+        let v6 = simpleGraph.createVertex(data: 6)
+        let v7 = simpleGraph.createVertex(data: 7)
+        simpleGraph.createEdges(from: v0, to: v3)
+        simpleGraph.createEdges(from: v1, to: v3)
+        simpleGraph.createEdges(from: v2, to: v4)
+        simpleGraph.createEdges(from: v2, to: v3)
+        simpleGraph.createEdges(from: v5, to: v6)
+        simpleGraph.createEdges(from: v5, to: v7)
+        simpleGraph.createEdges(from: v6, to: v7)
+        simpleGraph.createEdges(from: v7, to: v5)
+        let b = BipartiteGraph(graph: simpleGraph)
+        XCTAssertFalse(b.isBipartite())
+    }
 }
