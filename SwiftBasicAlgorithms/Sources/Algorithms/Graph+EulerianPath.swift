e9db03a39e90704c946b1872e6c7c2395ea0f622
punty <francesco.puntillo@gmail.com>
2018-12-17 17:13:29 +0000

    WIP: Fleury’s Algorithm

--- SwiftBasicAlgorithms/Sources/Algorithms/Graph+EulerianPath.swift
+++ SwiftBasicAlgorithms/Sources/Algorithms/Graph+EulerianPath.swift
@@ -9,7 +9,51 @@ extension Graph.EdgeList {
     }
 }
 
+
 extension Graph {
+    
+    private func nextEdge(start: Int) -> Int {
+        let edgeList = self[start]
+        if edgeList.degree == 1 {
+            guard let edge = edgeList.edges.first else {fatalError()}
+            return edge
+        }
+        
+        return -1
+    }
+    
+    private func countComponents(start: Int) -> Int {
+        var visited = Array<Bool>(repeating: false, count: count)
+        var components = 0
+        dfs(vertex: start, visited: &visited) { _, _ in
+            components += 1
+        }
+        return components
+    }
+    
+    
+    /*
+   
+     
+     // 2) If there are multiple adjacents, then
+     // u-v is not a bridge Do following steps
+     // to check if u-v is a bridge
+     // 2.a) count of vertices reachable from u
+     boolean[] isVisited = new boolean[this.vertices];
+     int count1 = dfsCount(u, isVisited);
+     
+     // 2.b) Remove edge (u, v) and after removing
+     //  the edge, count vertices reachable from u
+     removeEdge(u, v);
+     isVisited = new boolean[this.vertices];
+     int count2 = dfsCount(u, isVisited);
+     
+     // 2.c) Add the edge back to the graph
+     addEdge(u, v);
+     return (count1 > count2) ? false : true;
+     }
+     */
+    
     func eulerianPath() -> [Vertex]? {
         guard !isEmpty else { return nil }
         let oddCount = filter { $0.degree % 2 != 0 }.count
